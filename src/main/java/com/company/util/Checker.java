6b65414aeb5aa32656c7dcf6436f0e2f652753f6
di-menchy <70278272+di-menchy@users.noreply.github.com>
2020-09-23 16:26:48 +0200

    Update Checker.java
    код упрощен

--- src/main/java/com/company/util/Checker.java
+++ src/main/java/com/company/util/Checker.java
@@ -1,20 +1,19 @@
 package com.company.util;
 
 public class Checker {
+    public boolean isEven(int number) {  // четное
 
-        public boolean isEven(int number) {  // четное
-
-            if (Math.abs(number % 2) > 0)
-                return false;
-            else
-                return true;
-        }
+        if (number % 2 == 0)
+            return true;
+        else
+            return false;
+    }
 
 
-            public boolean isOdd(int number) {   //нечетное
-                if (Math.abs(number % 2) > 0)
-                    return true;
-                else
-                    return false;
-            }
-        }
+    public boolean isOdd(int number) {   //нечетное
+        if (number % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+}
